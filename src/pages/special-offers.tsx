b67dcbc075a5a66bae9e1a7c16e2f967882bbfeb
Shahnur Alam <shahnur@wafisolutions.com>
2023-11-02 12:47:20 +0600

    component dynamic
    component dynamic

--- src/pages/special-offers.tsx
+++ src/pages/special-offers.tsx
@@ -23,127 +23,18 @@ import useInfiniteScroll from "@/hooks/useInfiniteScroll";
 import { getOffers } from "@/queries/offers";
 import InfiniteScroll from "react-infinite-scroll-component";
 import OfferCard from "@/components/Card/OfferCard";
+import Head from "next/head";
+import FilterOffers from "@/components/Shared/FilterOffers";
+const priceRange = PriceRange;
 
 const specialOffers = () => {
 
 
-  const [termsAndConditionsModalData, setTermsAndConditionsModalData] =
-    useState(null);
-
-  const [openLoginModal, setOpenLoginModal] = useState<boolean>(false);
-  const [couponModalData, setCouponModalData] = useState<boolean>(false);
-
-  const { isLoading, cards, hasMore, fetchMoreData } =
-    useInfiniteScroll(getOffers);
-
-  // const departurePorts = DeparturePort;
-  // const cruiseDestinations = CruiseDestinations;
-  const priceRange = PriceRange;
-  //const seasons = SeasonData;
-  
-  const [selectedPort, setSelectedPort] = useState<any>(null);
-  const [selectedDestination, setSelectedDestination] = useState<any>(null);
-  const [selectedPriceRange, setSelectedPriceRange] = useState<any>(null);
-  const [selectedSeason, setSelectedSeason] = useState<any>(null);
-  const { isLoading: isLoadingDepartures, data: departures, refetch: refetchDepartures } = useQuery(
-    "departures",
-    () => getDepartures(),
-    {
-      refetchOnWindowFocus: false,
-      enabled: true,
-    }
-  );
-
-  const { isLoading: isLoadingSeasons, data: season, refetch: refetchSeasons } = useQuery(
-    "seasons",
-    () => getSeasons(),
-    {
-      refetchOnWindowFocus: false,
-      enabled: true,
-    }
-  );
-  const { isLoading: isLoadingDestinations, data: destinations, refetch: refetchDestinations } = useQuery(
-    "destinations",
-    () => getDestinations(),
-    {
-      refetchOnWindowFocus: false,
-      enabled: true,
-    }
-  );
-  // const { isLoading: isLoadingPrices, data: offers, refetch: refetchOffers } = useQuery(
-  //   "Offer",
-  //   () => getprices(),
-  //   {
-  //     refetchOnWindowFocus: false,
-  //     enabled: true,
-  //   }
-  // );
-
-  // console.log('s', departures);
-   console.log('s', season);
-    console.log('c',cards);
-
-  const departurePorts = departures?.map(({ id, attributes: { title } }) => ({
-    value: id,
-    label: title,
-  }));
-
-  const cruiseDestinations = destinations?.map(({ id, attributes: { title } }) => ({
-    value: id,
-    label: title,
-  }));
-
-  const seasons = season?.map(({ id, attributes: { title } }) => ({
-    value: id,
-    label: title,
-  }));
-  // const priceRange = offers?.map(({ id, attributes: { offer_price  } }) => ({
-  //   value: id,
-  //   label: offer_price
-  //   ,
-  // }));
-
-  // useEffect(() => {
-
-  //    const d = cards.filter(e => e?.attributes?.destinations?.data?.attributes?.title == selectedDestination?.lebel);
-  //    console.log(d);
-  // }, [selectedDestination])
-
-  const filteredCards = cards.filter((card) => {
-    if (selectedPort) {
-      return card?.attributes?.departure?.data?.attributes?.title === selectedPort.label;
-    }
-    
-    if (selectedDestination) {
-      const hasMatchingDestination = card?.attributes?.destinations?.data?.some(
-        (item) => item?.attributes?.title.includes(selectedDestination.label)
-      );
-      return hasMatchingDestination;
-    }
-    
-    
-    if (selectedPriceRange) {
-      return (
-        card?.attributes?.offer_price >= selectedPriceRange.min &&
-        card?.attributes?.offer_price <= selectedPriceRange.max
-      );
-    }
-    
-
-      
-    if (selectedSeason) {
-      return card?.attributes?.season?.data?.attributes?.title === selectedSeason.label;
-    }
-    
-    // If none of the conditions are met, include the card in the result.
-    return true;
-  });
-    
   return (
     <main>
-      {/* <Head>
-        <title>Cruise Cards</title>
-      </Head> */}
+      <Head>
+        <title>Special Offers</title>
+      </Head>
 
 
       <div className="flex flex-col px-3 md:px-[32px] lg:px-[75px] pt-4 md:pt-[75px]">
@@ -167,60 +58,55 @@ const specialOffers = () => {
             placerat lorem convallis.
           </p>
         </section>
-
-        <div className="text-3xl block lg:hidden mb-2">Filter by:</div>
-        <div className="grid grid-cols-2 lg:grid-cols-5 gap-3">
-          <div className="text-3xl hidden lg:block">Filter by:</div>
-
-          <Select
-            className="w-full"
-            defaultValue={selectedPort}
-            isClearable={true}
-            options={departurePorts}
-            placeholder="Departure ports"
-            onChange={(e) => setSelectedPort(e)}
-          />
-          <Select
-            className="w-full"
-            defaultValue={selectedDestination}
-            isClearable={true}
-            options={cruiseDestinations}
-            placeholder="Cruise destinations"
-            onChange={(e) => setSelectedDestination(e)}
-          />
-          <Select
-            className="w-full"
-            defaultValue={selectedPriceRange}
-            isClearable={true}
-            options={priceRange}
-            placeholder="Price range"
-            onChange={(e) => setSelectedPriceRange(e)}
-          />
-
-          <Select
-            className="w-full"
-            defaultValue={selectedSeason}
-            isClearable={true}
-            options={seasons}
-            placeholder="Season"
-            onChange={(e) => setSelectedSeason(e)}
-          />
-        </div>
       </div>
 
-      <section>
-        {/* <CruiseLineOffer /> */}
 
+      <FilterOffers finishedText="All special offers loaded" />
 
-        <main className="flex flex-col p-3 md:p-[32px] lg:p-[75px]">
-          {/* <Head>
-        <title>Cruise Cards</title>
-      </Head> */}
+      {/* <section className="p-3 md:p-[32px] lg:p-[75px]">
+        <div className="mb-8 md:mb-[75px]">
+          <div className="text-3xl block lg:hidden mb-2">Filter by:</div>
+          <div className="grid grid-cols-2 lg:grid-cols-5 gap-3">
+            <div className="text-3xl hidden lg:block">Filter by:</div>
 
-          {isLoading && <>Loading...</>}
+            <Select
+              className="w-full"
+              defaultValue={selectedPort}
+              isClearable={true}
+              options={departurePorts}
+              placeholder="Departure ports"
+              onChange={(e) => setSelectedPort(e)}
+            />
+            <Select
+              className="w-full"
+              defaultValue={selectedDestination}
+              isClearable={true}
+              options={cruiseDestinations}
+              placeholder="Cruise destinations"
+              onChange={(e) => setSelectedDestination(e)}
+            />
+            <Select
+              className="w-full"
+              defaultValue={selectedPriceRange}
+              isClearable={true}
+              options={priceRange}
+              placeholder="Price range"
+              onChange={(e) => setSelectedPriceRange(e)}
+            />
 
+            <Select
+              className="w-full"
+              defaultValue={selectedSeason}
+              isClearable={true}
+              options={seasons}
+              placeholder="Season"
+              onChange={(e) => setSelectedSeason(e)}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col">
           <div className="flex flex-col">
-
             <InfiniteScroll
               dataLength={cards?.length} //This is important field to render the next data
               next={fetchMoreData}
@@ -249,7 +135,7 @@ const specialOffers = () => {
             </InfiniteScroll>
           </div>
 
-          {/* Terms and conditions modal based on cruise line item */}
+
 
           {termsAndConditionsModalData && (
             <TermsAndConditionsCruiseLineModal
@@ -258,7 +144,7 @@ const specialOffers = () => {
             ></TermsAndConditionsCruiseLineModal>
           )}
 
-          {/* Login modal */}
+
           {openLoginModal && (
             <LoginModal
               openLoginModal={openLoginModal}
@@ -266,16 +152,15 @@ const specialOffers = () => {
             />
           )}
 
-          {/* Coupon modal */}
+
           {couponModalData && (
             <CouponModal
               couponModalData={couponModalData}
               setCouponModalData={setCouponModalData}
             />
           )}
-        </main>
-
-      </section>
+        </div>
+      </section> */}
     </main>
   );
 };
