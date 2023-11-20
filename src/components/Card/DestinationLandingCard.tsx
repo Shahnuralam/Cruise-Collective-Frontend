690144601ff238709c3d18de893ce5a32c1b3fb6
Shahnur Alam <shahnur@wafisolutions.com>
2023-11-20 18:46:12 +0600

    coming soon bold

--- src/components/Card/DestinationLandingCard.tsx
+++ src/components/Card/DestinationLandingCard.tsx
@@ -42,7 +42,7 @@ const DestinationCard = (props) => {
 
       <div className="card-container my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12">
         {source == "four" && slug !== "multi-continent" && !continentCountries?.length ? (
-          <p className="text-lg">Coming Soon</p>
+          <p className="text-lg font-bold">Coming Soon...</p>
         ) : (
           ""
         )}
